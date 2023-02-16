04a408b02c736bfe21bb46f6c63b6683ece736e2
mogesberhe <mogesberhe@gmail.com>
2023-02-16 11:02:27 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+i#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return (0);
+}
