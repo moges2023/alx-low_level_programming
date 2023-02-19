dde71223ffaef1e880a145dc1060da0fbe15ad00
mogesberhe <mogesberhe@gmail.com>
2023-02-19 10:41:30 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all strings from 0 to 9
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; nub++)
+		printf("%d", num);
+
+	pintf("\n");
+
+	return (0);
+}
