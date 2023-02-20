b51a49dbe6f0dbba0b6bf894127a5a731bcb627c
mogesberhe <mogesberhe@gmail.com>
2023-02-20 09:21:06 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desecription: 'prints nubmers of base 16'
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+
+	putchar('\n');
+	return (0);
+}
