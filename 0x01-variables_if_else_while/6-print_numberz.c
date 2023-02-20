28f7e4023c404ddd403d022d3574a2d74ea8ef4f
mogesberhe <mogesberhe@gmail.com>
2023-02-20 01:14:20 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * main - headers goes there
+ * discribtion:"prints numbers of base 10 using putchar"
+ * Return: always 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 0; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
