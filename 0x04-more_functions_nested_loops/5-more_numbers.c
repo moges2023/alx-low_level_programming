1d2556d5efbf355e74af5e6fb9b6c13077b4da52
mogesberhe <mogesberhe@gmail.com>
2023-02-24 04:23:11 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,18 @@
  */
 void more_numbers(void)
 {
-    int x, y;
-    for(x = 0; x < 10; x++)
-    {
-	    for (y = 0; y <= 14; y++)
-	    {
-	    if(y > 9)
-	    {
-	    _putchar((y/10) + '0');
-	    }
-	    _putchar((y % 10) + '0');
-	    }
-	    _putchar('\n');
-	    }
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
