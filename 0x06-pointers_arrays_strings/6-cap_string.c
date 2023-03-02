6a77ae0667558037296982cb4957dfb026de3376
mogesberhe <mogesberhe@gmail.com>
2023-03-02 07:42:21 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,12 +17,12 @@ char *cap_string(char *str)
 		if (str[index - 1] == ' ' ||
 		str[index - 1] == '\t' ||
 		str[index - 1] == '\n' ||
-		str[index - 1] == '.' ||
+		str[index - 1] == ',' ||
 		str[index - 1] == ';' ||
 		str[index - 1] == '.' ||
 		str[index - 1] == '!' ||
 		str[index - 1] == '?' ||
-		str[index - 1] == '-' ||
+		str[index - 1] == '"' ||
 		str[index - 1] == '(' ||
 		str[index - 1] == ')' ||
 		str[index - 1] == '{' ||
