5e8ca120623f80c69489009c108833cfbb37a8fc
mogesberhe <mogesberhe@gmail.com>
2023-03-02 04:31:16 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
-#define main_H
-
+#define MAIN_H
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -12,7 +11,6 @@ char *cap_string(char *a);
 char *leet(char *a);
 char *rot13(char *a);
 void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-void print_buffer(char *b, int size);
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b,int size);
 #endif
